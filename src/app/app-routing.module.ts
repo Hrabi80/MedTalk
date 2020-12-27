762853eaf29c26c7f58e88ab3707f1bae0fdfd85
Hrabi <Hrabbi.ahmed88@gmail.com>
2020-12-27 05:33:58 +0100

    05:33

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subred
 import { ViewPostComponent } from './post/view-post/view-post.component';
 import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 import { AuthGuard } from './auth/auth.guard';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,7 +19,12 @@ const routes: Routes = [
   { path: 'create-post', component: CreatePostComponent, /*canActivate: [AuthGuard]*/ },
   { path: 'create-subreddit', component: CreateSubredditComponent, /*canActivate: [AuthGuard]*/ },
   { path: 'sign-up', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: SidebarComponent,
+   children:[
+    { path: 'create-post', component: CreatePostComponent, /*canActivate: [AuthGuard]*/ },
+   ] },
+
 ];
 
 @NgModule({
