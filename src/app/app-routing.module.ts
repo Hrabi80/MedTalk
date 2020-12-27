a03b6970872bd2b6a0035e67ee94b122a14802c7
Hrabi <Hrabbi.ahmed88@gmail.com>
2020-12-27 02:59:04 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,10 @@ import { AuthGuard } from './auth/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'view-post/:id', component: ViewPostComponent },
-  { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: 'user-profile/:name', component: UserProfileComponent, /*canActivate: [AuthGuard] */},
   { path: 'list-subreddits', component: ListSubredditsComponent },
-  { path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard] },
-  { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [AuthGuard] },
+  { path: 'create-post', component: CreatePostComponent, /*canActivate: [AuthGuard]*/ },
+  { path: 'create-subreddit', component: CreateSubredditComponent, /*canActivate: [AuthGuard]*/ },
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent }
 ];
